2a55d1df63947b1e3ef3e7b9dae6eefcb56bd9e9
Chengcheng-S <87012234+Chengcheng-S@users.noreply.github.com>
2024-09-29 14:26:09 +0800

    fix build.sh

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 #exit on error
 set -e
 
-xcrun -sdk macosx metal -c src/shaders.metal -o src/shaders.air
-xcrun -sdk macosx metallib src/shaders.air -o src/shaders.metallib
+xcrun -sdk macosx metal -c ./src/shaders.metal -o ./src/shaders.air
+xcrun -sdk macosx metallib ./src/shaders.air -o ./src/shaders.metallib
