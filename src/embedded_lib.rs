776aa568154b92b1ba2b5f74a84fececa6931537
Chengcheng-S <87012234+Chengcheng-S@users.noreply.github.com>
2024-09-30 10:34:12 +0800

    adjust the project structure

--- src/embedded_lib.rs
+++ src/embedded_lib.rs
@@ -2,7 +2,7 @@ use metal::*;
 use objc::rc::autoreleasepool;
 
 pub fn embedded() {
-    let library_data = include_bytes!("shaders.metallib");
+    let library_data = include_bytes!("../build_miden/shader/shaders.metallib");
 
     autoreleasepool(|| {
         let device = Device::system_default().expect("no device found");
